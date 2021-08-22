ac1b59da4fb92047b9aaf28875456aff9e33c88e
davidwcheng <68088328+davidwcheng@users.noreply.github.com>
2021-08-22 16:13:28 -0400

    added inputfield and urql for registration

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -2,16 +2,19 @@ import React from "react";
 import Box from '@material-ui/core/Box'
 import { styled } from "@material-ui/core";
 
-interface WrapperProps { }
+interface WrapperProps {
+  variant?: "small" | "regular" //optional size for user registration
+}
 
 const MyBox = styled(Box)({
   background: 'radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%)',
 
 })
-export const Wrapper: React.FC<WrapperProps> = ({ children }) => {
+export const Wrapper: React.FC<WrapperProps> = ({ children, variant = "regular" }) => {
   return (
-    <MyBox 
-      maxWidth="800px"
+    <MyBox
+      maxWidth={variant === "regular" ? "800px" : "400px"}
+      width="100%"
       marginX="auto"
       marginTop={8}
       borderRadius={8}
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,35 +1,44 @@
 import React from "react";
 import { Form, Formik } from "formik";
-import {
-  FormControl,
-  FormLabel,
-  Input,
-} from "@chakra-ui/react";
 import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
+import { Box, Button } from "@material-ui/core";
+
 interface registerProps { }
 
 export const Register: React.FC<registerProps> = ({ }) => {
   return (
-    <Wrapper>
+    <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
         onSubmit={(values) => {
           console.log(values);
         }}
       >
-        {(values, handleChange) => (
+        {() => (
           <Form>
-            <FormControl>
-              <FormLabel htmlFor="username" color="blue.700" fontSize="20px">Username</FormLabel>
-              <Input
-                color="blue.700"
-                value={values.username}
-                onChange={handleChange}
-                id="username"
-                placeholder="username"
-              />
-              {/* <FormErrorMessage>{form.errors.name}</FormErrorMessage> */}
-            </FormControl>
+            <InputField
+              name="username"
+              placeholder="username"
+              label="Username">
+            </InputField>
+            <Box marginTop={4}>
+              <InputField
+                name="password"
+                placeholder="password"
+                label="Password"
+                type="password">
+              </InputField>
+            </Box>
+            <Box mt={4}>
+              <Button
+                type="submit"
+                variant="outlined"
+                color="primary"
+              >
+                Register
+              </Button>
+            </Box>
           </Form>
         )}
       </Formik>
